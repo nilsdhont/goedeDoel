cddaedf8f6faf1b9dbab4e165f5c922171b54c97
nilsdhont <dhont_nils@hotmail.com>
2016-03-09 11:21:24 +0100

    extra tests

--- src/test/java/be/vdab/entities/GoedeDoelTest.java
+++ src/test/java/be/vdab/entities/GoedeDoelTest.java
@@ -1,6 +1,7 @@
 package be.vdab.entities;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.math.BigDecimal;
 
@@ -8,20 +9,31 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GoedeDoelTest {
-	private static final String NAAM ="CliniClowns"; 
+	private static final String NAAM = "CliniClowns";
 	private GoedeDoel doel;
 
 	@Before
 	public void before() {
 		doel = new GoedeDoel(NAAM);
 	}
+
 	@Test
 	public void getNaam() {
 		assertEquals(NAAM, doel.getNaam());
 	}
-	
+
 	@Test
 	public void eenNieuwDoelHeeftNogNietsOpgebracht() {
-	assertEquals(0, doel.getOpgebracht().compareTo(BigDecimal.ZERO));
+		assertEquals(0, doel.getOpgebracht().compareTo(BigDecimal.ZERO));
+	}
+
+	@Test
+	public void goedeDoelenMetDezelfdeNaamMoetenVolgensEqualsGelijkZijn() {
+		assertEquals(new GoedeDoel("CLINICLOWNS"), doel);
+	}
+
+	@Test
+	public void goedeDoelenMetVerschillendeNaamMoetenVolgensEqualsVerschillendZijn() {
+		assertNotEquals(new GoedeDoel("Unicef"), doel);
 	}
 }
